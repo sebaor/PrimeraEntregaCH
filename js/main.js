815caf45e1bf79089c4ea14dbb633821c664abf1
Sebastian Pereira <sebastianpereira877@gmail.com>
2023-10-15 15:41:30 -0300

    primera entrega ch

---
+++ js/main.js
@@ -0,0 +1,40 @@
+let ingresoInvalido = true;
+
+while(ingresoInvalido){
+    let nombreDeusuario = prompt("Ingrese su nombre de usuario para iniciar sesion");
+    if(nombreDeusuario !=""){
+        alert("¡Bienvenid@ a PayDay " + nombreDeusuario +"!");
+        ingresoInvalido = false;
+    }else{
+        alert("Campos vacíos");
+        break;
+    }
+}
+if(ingresoInvalido == true){
+    alert("Vuelve a intentarlo por favor");
+}else{
+    alert("En PayDay nos dedicamos a liquidar sueldos nominales en $");
+}
+
+//funcion para calcular descuentos de sueldos nominales
+function calcularSueldo (sueldoNominal, descuentoMontepio = 0.15, descuentoFRL = 0.010, descuentoFonasa = 0.045){
+    let descuentosTotales = sueldoNominal * descuentoMontepio + sueldoNominal * descuentoFRL + sueldoNominal * descuentoFonasa;
+    alert("El descuento es de: $" + descuentosTotales);
+    return sueldoNominal - descuentosTotales;
+}
+
+let confirmacionDeusuario = true;
+
+while(confirmacionDeusuario){
+    let ingresoDesueldo = parseFloat(prompt("Ingrese por favor un sueldo en $ nominales"));
+    let sueldoLiquido = calcularSueldo (ingresoDesueldo);
+    alert("Su sueldo liquido a cobrar es de: $" + sueldoLiquido);
+    let respuestaconfirmacionDeusuario = prompt("¿Desea calcular otro sueldo? | Ingrese SI o NO");
+    if(respuestaconfirmacionDeusuario == "NO"){
+        confirmacionDeusuario = false;
+    }
+}
+
+if(confirmacionDeusuario == false){
+    alert("Vuelve pronto");
+}
